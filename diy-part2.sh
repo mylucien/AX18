127f0a0ea0849d6b0a273e27590c9d66b2b48248
mylucien <53183558+mylucien@users.noreply.github.com>
2022-08-29 10:02:24 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.8.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把 GL-AX1800 修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i 's/OpenWrt/GL-AX1800/g' package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/GL-AX1800/g' package/base-files/files/bin/config_generate
