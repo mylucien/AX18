575c56430986ff7cbba18e77deed184df48b2248
mylucien <53183558+mylucien@users.noreply.github.com>
2022-08-29 10:01:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,5 +17,3 @@
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
-echo 'src-git openclash https://github.com/vernesong/OpenClash.git' >>feeds.conf.default
-
